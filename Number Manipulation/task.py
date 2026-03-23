54cdf0537bbc1cc9dc98fc0c16e02f814a37caf7
Megha Nair <101555874+octoberraiin@users.noreply.github.com>
2026-03-23 19:34:06 +0530

    Add files via upload
    Day 2 of #100DaysOfCode 🚀-> 1.5 hours.
    Today was about:
      • Data Types
      • Type conversion
      • F-strings
      • Fubscripting
    I made a Tip Calculator Project 🧠

---
+++ Number Manipulation/task.py
@@ -0,0 +1,8 @@
+bmi = 84 / 1.65 ** 2
+
+score=0
+score+=1
+print(score)
+
+#f-strings
+print("Your score is {score}".format(score=score))
---
+++ Type Error, Checking and Conversion/task.py
@@ -0,0 +1,8 @@
+print(type("2.3"))
+print(type(2))
+print(type(2.3))
+print(type(True))
+
+print(int("234") + int("4"))
+
+print("Number of letters in your name: " + str(len(input("Enter your name"))))
