720ce315f10443ac73e5ccc6a874db068e85add3
Megha Nair <101555874+octoberraiin@users.noreply.github.com>
2026-03-23 19:37:44 +0530

    Delete Type Error, Checking and Conversion directory

--- Type Error, Checking and Conversion/task.py
+++
@@ -1,8 +0,0 @@
-print(type("2.3"))
-print(type(2))
-print(type(2.3))
-print(type(True))
-
-print(int("234") + int("4"))
-
-print("Number of letters in your name: " + str(len(input("Enter your name"))))
