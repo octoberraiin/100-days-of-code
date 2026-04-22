19f400ea6351c01652eee884f7053e792f06fae7
Megha Nair <101555874+octoberraiin@users.noreply.github.com>
2026-04-22 12:10:20 +0530

    Day 21 of #100DaysOfCode -> 1.5 hours.
    Today, I completed my snake game program!
    #100DaysOfCode

---
+++ snake_game/scoreboard.py
@@ -0,0 +1,29 @@
+from turtle import Turtle
+
+FONT = ("Courier", 20, "normal")
+ALIGNMENT = "center"
+
+
+class Scoreboard(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.color("white")
+        self.hideturtle()
+        self.penup()
+        self.goto(0, 250)
+
+        self.score = 0
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.write(f"Score = {self.score}", align=ALIGNMENT, font=FONT)
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write("Game Over", align=ALIGNMENT, font=FONT)
+
+    def increase_score(self):
+        self.score += 1
+        self.clear()
+        self.update_scoreboard()
